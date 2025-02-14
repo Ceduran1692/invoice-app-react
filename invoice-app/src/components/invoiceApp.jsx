4c4bb06a9ac869935060b6c69b66caf49eb52d2b
ceduran <carlos.duran@besysoft.com>
2025-02-14 17:26:08 -0300

    Integracion de bootstrap

--- invoice-app/src/components/invoiceApp.jsx
+++ invoice-app/src/components/invoiceApp.jsx
@@ -5,58 +5,90 @@ export const InvoiceApp=()=>{
     const {country, street, city}= client.address;
     const{product, price, quantity}= items;
 
-    return <>
-        <h1>Invoice App</h1>
-
-        <h2>Bill</h2>
-        <ul>
-            <li>id: {id}</li>
-            <li>name: {name}</li>
-        </ul>
-        <h3>Client data</h3>
-        <ul>
-            <li>
-                Client: {client.name +' '+ client.lastname} 
-            </li>
-            <li>
-                 Address:
-                <ul>
-                    <li>
-                        Country: {country}
-                    </li>
-                    <li>
-                        City: {city}
-                    </li>
-                    <li>
-                        Street: {street}
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <h3>Items</h3>
-        
-        {items.map((element,index) => (
-            <ul key={index}>
-                <li>
-                    product: {element.product}
-                </li>
-                <li>
-                    price: {element.price}
-                </li>
-                <li>
-                    quantity: {element.quantity}
-                </li>
-            </ul>
-        ))}
-
-        <h2>Fiscal Data</h2>
-        <ul>
-            <li>
-                Company: {company.name}
-            </li>
-            <li>
-                Fiscal number: {company.fiscalNumber}
-            </li>
-        </ul>
+    return(
+     <>
+     <div class= "card ms-1 me-1">
+        <div class= "card-header">
+            <h1>Invoice App</h1>
+        </div> 
+        <div class="card-body">
+            <div class= "container">
+                <div class="row">
+                    <h2 class="card-title">Bill</h2>
+                    <ul class="list-group">
+                        <li class="list-group-item">id: {id}</li>
+                        <li class="list-group-item">name: {name}</li>
+                    </ul>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h3 class="card-title">Client data</h3>
+                        <ul class="list-group">
+                            <li class="list-group-item active">
+                                Client: {client.name +' '+ client.lastname} 
+                            </li>
+                            <li class="list-group-item">
+                                Address:
+                                <ul >
+                                    <li >
+                                        Country: {country}
+                                    </li>
+                                    <li>
+                                        City: {city}
+                                    </li>
+                                    <li >
+                                        Street: {street}
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h2 class="card-title">Fiscal Data</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item active">
+                                Company: {company.name}
+                            </li>
+                            <li class="list-group-item">
+                                Fiscal number: {company.fiscalNumber}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <h3 class="card-title text-center" >Items</h3>
+                    <div class="container">
+                        <div class="border-round">
+                            <table class="table border rounded">
+                                <thead class="table-info">
+                                    <tr>
+                                        <th>Product</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {items.map((element,index) => (
+                                        <tr key={index}>
+                                            <td>
+                                                {element.product}
+                                            </td>
+                                            <td>
+                                                {element.price}
+                                            </td>
+                                            <td>
+                                                {element.quantity}
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>                            
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </>
+    )
 }
